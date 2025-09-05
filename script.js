68cdb700736f2718e8af03bcd70664b607ce4319
jeankelcio <jeankelciocarvalho@gmail.com>
2025-09-04 22:07:37 -0300

    variaveis

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-alert("Ola mundo")
+// alert("Ola mundo")
+// alert("Oi")
+// alert("Tudo bem?")
+// alert("Voçe vem sempre aqui?")
 
+// let idade
+
+// idade = 18
+// alert(idade)
+
+// idade = "dezoito"
+// alert(idade)
+
+const amor = "meu pimeiro amor"
+alert(amor)
+
+amor = "meu segundo amor"
+alert(amor)
+
+
+var antigao = "nunca usar isso!"
 
