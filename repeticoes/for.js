c723bce593c79ef3b202ada4c45bc6053fc94c40
JEAN KELCIO CARVALHO <jean_k_carvalho@estudante.sesisenai.org.br>
2025-10-02 21:27:17 -0300

    repeticoes

---
+++ repeticoes/for.js
@@ -0,0 +1,28 @@
+
+// let i = 0
+// while(i<3){
+//     alert("Valor do i: " + i)
+//     i++
+// }
+
+// for(let i = 0; i<3; i++){
+
+// }
+
+// let = soma 0
+// let nota1 = Number(prompt("Digite a nota: "))
+// soma = nota1
+// let nota2 = 
+// let = soma
+// for(let i=0; i<10; i++){
+//     let nota = Number(prompt("Digite a "+ (i+i) + "a nota: "))
+//     soma = soma + nota
+// }
+
+// alert("soma: " + soma)
+
+// const nomes = ["Dona maria", "Nano Juca", "Lucio Fernando", "Capitão ganso" ]
+
+// for ( i=0; i<4; i++){
+//     alert(nomes[i])
+// }
